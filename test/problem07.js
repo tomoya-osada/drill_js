52228b4f97f88d522b139b58b0919c2cebdeb112
tosada <tomoya.a.osada@gmail.com>
2017-05-25 01:58:00 +0900

    7

---
+++ test/problem07.js
@@ -0,0 +1,11 @@
+// test/someTest.js
+const assert = require("assert")
+const myMethod = require("../07_LowString")
+
+describe("probrems6", () => {
+  it("文字列を受け取って、文字列全体の長さを返す", () => {
+      const input = "GITHUB"
+      const output = myMethod(input)
+      assert.equal("GItHUB" , output) // Array, Objectの場合はassert.deepEqual。
+  })
+})
