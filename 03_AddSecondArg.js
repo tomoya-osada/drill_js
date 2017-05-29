6b58d12e07ab7e753637488e48e73e3a1aac48bd
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-29 18:44:02 +0900

    fix 3

--- 03_AddSecondArg.js
+++ 03_AddSecondArg.js
@@ -1,11 +1,5 @@
 function AddSecondArg(str1, str2){
-  let result = str1
-
-  for(let i = 0; i < 3; i++ ){
-    result += ` ${str2}`
-  }
-
-  return result
+  return str1 + ` ${str2}`.repeat(3)
 }
 
 
