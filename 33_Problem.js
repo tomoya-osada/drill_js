5a51875dda7635f6d44c99e6ca4d6c1b50598e22
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 10:58:42 +0900

    問題33

---
+++ 33_Problem.js
@@ -0,0 +1,10 @@
+// 入力: Array[Int]
+// 出力: 3の倍数のみ'banana'’に変換した配列
+
+function changeBanana(arr){
+    return arr.map((e) => {
+        return e % 3 === 0 ? "banana" : e
+    })
+}
+
+module.exports = changeBanana
