e38b2cc0d77e7c3957f7ca840ffb1d34c65c9a60
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 13:27:03 +0900

    初期値入れます

--- 13_Problem.js
+++ 13_Problem.js
@@ -1,7 +1,7 @@
 // reduce関数を使い、range関数で作成した結果の合計を計算せよ
 function ArrayRangeSum(start, itemLength){
     const arrayRange = Array.from({length: itemLength}, (v, i) => start + i)
-    return arrayRange.reduce((previousValue, currentValue) => previousValue + currentValue)
+    return arrayRange.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
 }
 
 module.exports = ArrayRangeSum
