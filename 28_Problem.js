19e0a9f401a5b6734336bb259025cc3d5b13cef0
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 10:24:45 +0900

    愚直version

--- 28_Problem.js
+++ 28_Problem.js
@@ -1,9 +1,10 @@
 // 入力: Array1, Array2
 // 処理: ２つの配列の共通要素を配列として返す
 
-function ReturnCommonArray(array1, array2){
-    const mergeArray = array1.concat(array2) 
-    console.log(mergeArray)
+function ReturnCommonArray(arr1, arr2){
+    const excludeDuplicatedArr1 = arr1.filter((e, i, self) => self.indexOf(e) === i);
+    const excludeDuplicatedArr2 = arr2.filter((e, i, self) => self.indexOf(e) === i);
+    const mergeArray = excludeDuplicatedArr1.concat(excludeDuplicatedArr2)
     return mergeArray.filter((e, i, self) => self.indexOf(e) ===i && i !== self.lastIndexOf(e))
 }
 
--- test/problem28.js
+++ test/problem28.js
@@ -3,7 +3,7 @@ const myMethod = require("../28_Problem")
 
 describe("problems28", () => {
   it("２つの配列の共通要素を配列として返す", () => {
-      const input1 = [12, 39, 99, 100, 50]
+      const input1 = [100, 39, 99, 100, 50]
       const input2 = [11, 39, 2, 50, 17]
       const output = myMethod(input1, input2)
       assert.deepEqual([39, 50] , output) // Array, Objectの場合はassert.deepEqual。
