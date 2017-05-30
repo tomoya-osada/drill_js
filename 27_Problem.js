218bc742ef01a6795776afe60c8362040789698a
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 09:59:31 +0900

    修正

--- 27_Problem.js
+++ 27_Problem.js
@@ -1,11 +1,11 @@
 // 入力: Array
-// 処理: 3番目と5番目の要素が同じかを判定する。要素数が5以下の時はfalseを返す 
+// 処理: 3番目と5番目の要素が同じかを判定する。要素数が5以下の時はfalseを返す
 
-function CompareIndex(array){
-    if (array.lengh <= 5){
-        return false 
+function CompareIndex(arr){
+    if (arr.length <= 5){
+        return false
      }
-    return array[2] === array[4]
+    return arr[2] === arr[4]
 }
 
 module.exports = CompareIndex
