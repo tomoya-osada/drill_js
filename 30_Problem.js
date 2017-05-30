9e78bd6f103d86fbc03e09e4a8c91d9abaaae421
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 09:56:51 +0900

    変数名変更

--- 30_Problem.js
+++ 30_Problem.js
@@ -1,7 +1,7 @@
 // 入力: Array
 // 出力: 入力されたarrayを3回繰り返したarray
-function RepeatArray(array){
-    const nestedArray = Array.from({length: 3}, (v) => array)
+function RepeatArray(arr){
+    const nestedArray = Array.from({length: 3}, (v) => arr)
     return nestedArray.reduce((a, b) => { return a.concat(b) })
 }
 
