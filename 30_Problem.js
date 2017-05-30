201b80deabdf1cf826c62f10f81d87d007cd4116
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 15:16:32 +0900

    修正

--- 30_Problem.js
+++ 30_Problem.js
@@ -2,7 +2,7 @@
 // 出力: 入力されたarrayを3回繰り返したarray
 function RepeatArray(arr){
     const nestedArray = Array.from({length: 3}, (v) => arr)
-    return nestedArray.reduce((a, b) => { return a.concat(b) })
+    return nestedArray.reduce((a, b) => { return [...a].concat(b) }, [])
 }
 
 module.exports = RepeatArray
