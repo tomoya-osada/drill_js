3255a6853529cafd8f0f69a407e9f9b704c7db3b
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 13:33:49 +0900

    配列展開verion

--- 15_Problem.js
+++ 15_Problem.js
@@ -1,8 +1,8 @@
 // 入力: Array
 // 先頭に'HEAD!'を挿入した結果を返す
 function InsertHead(array){
-    array.unshift("HEAD!")
-    return array
+    // array.unshift("HEAD!")
+    return ["HEAD!", ...array]
 }
 
 module.exports = InsertHead
