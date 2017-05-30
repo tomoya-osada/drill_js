c2f7059244bf8ccc22eefff2a4930114b41cc634
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 10:47:33 +0900

    修正

--- test/problem32.js
+++ test/problem32.js
@@ -3,8 +3,8 @@ const myMethod = require("../32_Problem")
 
 describe("problems32", () => {
   it(" -で連結した文字列", () => {
-      const input = ["お金も","土地も","ほしいよ"]
+      const input = ["お金も","土地も","ほしい"]
       const output = myMethod(input)
-      assert.equal("お金も-土地も-ほしいよ" , output) // Array, Objectの場合はassert.deepEqual。
+      assert.equal("お金も-土地も-ほしい" , output) // Array, Objectの場合はassert.deepEqual。
   })
 })
