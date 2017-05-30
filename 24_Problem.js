ea8cc155dd4000478f2da89cdb588bab85945189
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 14:49:02 +0900

    splice

--- 24_Problem.js
+++ 24_Problem.js
@@ -1,13 +1,16 @@
 // 入力: Array
 // 処理: 各要素のindexを3つふやす
-function IndexOfArray(array){
-    return array.map((value, index, self) => {
-        if (self[index+2] <= self.length){
-            return self[index+2]
-        }else{
-            return self[index-3]
-        }
-    })
+function IndexOfArray(arr){
+    const arrSplice = arr.splice(0, 2)
+    return arr.concat(arrSplice)
+
+    // return array.map((value, index, self) => {
+    //     if (self[index+2] <= self.length){
+    //         return self[index+2]
+    //     }else{
+    //         return self[index-3]
+    //     }
+    // })
 }
 
 module.exports = IndexOfArray
