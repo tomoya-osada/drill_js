de3268cab2967f864958acf464fd20933da05a1b
Tomoya Osada <tomoya.osada@livesense.co.jp>
2017-05-30 13:48:41 +0900

    テスト文言修正

--- test/problem20.js
+++ test/problem20.js
@@ -1,8 +1,8 @@
 const assert = require("assert")
 const myMethod = require("../20_Problem")
 
-describe("problemsXX", () => {
-  it("# XX", () => {
+describe("problems20", () => {
+  it("重複要素が存在するかを判定する", () => {
       const input = ["hoge", "hoge", "fuga", "foo"]
       const output = myMethod(input)
       assert.equal(true , output) // Array, Objectの場合はassert.deepEqual。
